dcb372636cae36c5abb955fab5b4edd5fe1258a9
Gabriel Moneta <gabrielpmoneta@gmail.com>
2025-10-26 14:42:55 -0300

    criação do DTO do ingresso

---
+++ src/main/java/com/encenape/dto/IngressoResponseDTO.java
@@ -0,0 +1,31 @@
+package com.encenape.dto;
+
+import com.encenape.model.Ingresso;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class IngressoResponseDTO {
+
+    private Long id;
+    private LocalDateTime dataCompra;
+    private Long eventoId;
+    private String eventoNome;
+    private LocalDateTime eventoData;
+    private String eventoLocal;
+    // TODO: adicionar status, qr code, etc.
+
+    // Construtor que pega o Ingresso e extrai informações do Evento associado
+    public IngressoResponseDTO(Ingresso ingresso) {
+        this.id = ingresso.getId();
+        this.dataCompra = ingresso.getDataCompra();
+        if (ingresso.getEvento() != null) {
+            this.eventoId = ingresso.getEvento().getId();
+            this.eventoNome = ingresso.getEvento().getNome();
+            this.eventoData = ingresso.getEvento().getData();
+            this.eventoLocal = ingresso.getEvento().getLocal();
+        }
+    }
+}
